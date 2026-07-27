eabcba4e21348101f3b324af3b0dfd9f822e7ec9
mateo <levvycum@gmail.com>
2026-07-27 01:05:47 -0300

    a

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,7 +5,7 @@ import yerbaTexture from "@/assets/yerba-texture.jpg";
 import { SITE, PRODUCTS, formatPrice, waLink } from "@/lib/site";
 import { WhatsAppIcon } from "@/components/site-layout";
 import { PhotoCarousel } from "@/components/photo-carousel";
-import favicon from "@/assets/logo-rm.jpg";
+import favicon from "@/assets/logo-rm.png";
 
 export const Route = createFileRoute("/")({
   head: () => ({
